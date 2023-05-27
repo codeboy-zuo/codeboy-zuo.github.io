42b157baa27bb532ba067f3da05e153f2072c172
codeboy0 <930541785@qq.com>
2023-05-27 23:22:33 +0800

    Site updated: 2023-05-27 23:22:28

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["post/19236.html","post/21442.html","post/30229.html","post/4333.html","post/47479.html","post/46969.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
