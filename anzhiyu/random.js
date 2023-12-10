380d35a86bc6015d8d4623213d002a7b9e0c262b
codeboy0 <930541785@qq.com>
2023-12-10 15:47:20 +0800

    Site updated: 2023-12-10 15:47:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/09/Butterfly主题优化/","2023/05/21/abbrlink/","2023/05/08/markdown/","2023/05/13/github-calendar/","2023/05/21/music/","2023/05/21/newPostIcon/"];function toRandomPost(){
+var posts=["2023/05/09/Butterfly主题优化/","2023/05/13/github-calendar/","2023/05/21/abbrlink/","2023/05/08/markdown/","2023/05/21/music/","2023/05/21/newPostIcon/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
