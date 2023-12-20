ec337173722e112aca4f5003a720252ebc18a4b4
codeboy0 <930541785@qq.com>
2023-12-20 19:50:18 +0800

    Site updated: 2023-12-20 19:50:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/46969.html","post/30229.html","post/19236.html","post/47479.html","post/4333.html","post/21442.html"];function toRandomPost(){
+var posts=["post/30229.html","post/46969.html","post/19236.html","post/47479.html","post/21442.html","post/4333.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
