352b1f0f2a9d7e95127cfad55c462d4ed364518a
codeboy0 <930541785@qq.com>
2024-07-07 16:45:19 +0800

    Site updated: 2024-07-07 16:45:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/46969.html","post/30229.html","post/21442.html","post/47479.html","post/4333.html","post/62556.html","post/19236.html","post/57921.html"];function toRandomPost(){
+var posts=["post/46969.html","post/30229.html","post/47479.html","post/19236.html","post/62556.html","post/21442.html","post/4333.html","post/57921.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
